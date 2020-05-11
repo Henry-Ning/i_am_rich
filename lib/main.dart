c94b877282eadd0a174ec85b70c10025c375e36d
Henry-Ning <henryhuning@gmail.com>
2020-05-11 06:44:08 -0700

    4th Commit 643AM 5-11-2020

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,13 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('I AM RICH'),
+          title: Text('I AM RICH'
+          ),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
           child: Image(
-            image: NetworkImage("""https://pbs.twimg.com/profile_images/875749462957670400/T0lwiBK8_400x400.jpg"""
+            image: AssetImage('images/diamond.png'
             ),
           ),
         ),
